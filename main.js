d595661a72ad50e82a6ba1c474c534aab910d8cb
eradoria <eradoria@gmail.com>
2022-04-30 16:39:31 -0500

    pre-final

--- main.js
+++ main.js
@@ -34,33 +34,48 @@ const printStacks = () => {
 // Next, what do you think this function should do?
 const movePiece = (startStack, endStack) => {
   // Your code here
+  //this will store the disc when it is remove from the stack array
   let lastItem = stacks[startStack].pop();
-
+  //this tells it to be put back into the new stack array it was moved too
   stacks[endStack].push(lastItem);
-  
+  //after every move it will check for a win
   checkForWin()
 
 }
 
 // Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
-const isLegal = (firstMove,secondMove) => {
-  // Your code here
-  if(!(stacks[secondMove].length) || (stacks[secondMove][stacks[secondMove].length -1] > stacks[firstMove][stacks[firstMove].length -1])){
-    return true 
-  } else {
+const isLegal = (startStack, endStack) => {
+  // console.log(stacks[endStack].length)
+  // console.log(stacks[startStack][stacks[startStack].length -1])
+
+  ////needs to convert the input to lower case and fail if its not
+  startStack = startStack.toLowerCase().trim(); 
+  endStack = endStack.toLowerCase().trim(); 
+  if((startStack !== 'a' && startStack !== 'b' && startStack !== 'c') || (endStack !== 'a' && endStack !== 'b' && endStack !== 'c')){
     return false
   }
 
+  //// lots of math but should know if the disc being moved it greater then the below it
+  if(stacks[endStack].length === 0 || stacks[startStack][stacks[startStack].length -1] < stacks[endStack][stacks[endStack].length -1]){
+      return true;
+    } else {
+      return false
+    }
+  }
+  
+ 
+
+  // Your code here
   // if (endStack[0] > endStack[1]) {
   //   return true
   // } else {
   //   return false
   // }
-}
 
 // What is a win in Towers of Hanoi? When should this function run?
 const checkForWin = () => {
   // Your code here
+  // should know when stack B or C has all 4 disc
   if (stacks.a.length === 0 && stacks.b.length === 4 || stacks.c.length === 4) {
       return true
   } else {
@@ -70,8 +85,15 @@ const checkForWin = () => {
 
 // When is this function called? What should it do with its argument?
 const towersOfHanoi = (startStack, endStack) => {
-   movePiece(startStack,endStack);
+//if the move is legal it will continue
+  if (isLegal(startStack, endStack)) {
+  //if it islegal is true then it call movepiece 
+    movePiece(startStack,endStack);
+  }else{
+    return false
+  }
 }
+
 
 const getPrompt = () => {
   printStacks();
@@ -111,12 +133,39 @@ if (typeof describe === 'function') {
       };
       assert.equal(isLegal('a', 'c'), true);
     });
+    //it should only take 1 letter from the user
+    it('should only take 1 letter entry', () => {
+      stacks = {
+        a: [4, 3, 2, 1],
+        b: [],
+        c: []
+      };
+      assert.equal(isLegal('a','b', 'c'), true);
+    });
+    // it should only take entry in lower case 
+    it('should only take in lowercase entry',function(){
+      stacks = {
+        a: [4, 3, 2, 1],
+        b: [],
+        c: []
+      };
+      let actual = isLegal('A   ', '  B');
+      let expected = true;
+      assert.equal(actual, expected); 
+    });
   });
   describe('#checkForWin()', () => {
     it('should detect a win', () => {
       stacks = { a: [], b: [4, 3, 2, 1], c: [] };
       assert.equal(checkForWin(), true);
       stacks = { a: [1], b: [4, 3, 2], c: [] };
+      assert.equal(checkForWin(), false);
+    });
+    //kinda an easy one but it should let stack C win also
+    it('should detect a win on stack C', () => {
+      stacks = { a: [], b: [4, 3, 2, 1], c: [] };
+      assert.equal(checkForWin(), true);
+      stacks = { a: [1], b: [], c: [4, 3, 2] };
       assert.equal(checkForWin(), false);
     });
   });
